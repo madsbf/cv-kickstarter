e4d87f76b4956b8f4c0719c124848e90d1803c34
anderslime <aemilnielsen@gmail.com>
2014-10-20 18:12:42 +0200

    Move and rename course xml to show it belongs to tests

--- dtu_course_base/test_dtu_course_base.py
+++ dtu_course_base/test_dtu_course_base.py
@@ -20,7 +20,7 @@ expected_course_objectives = [
 
 
 def first_course(language):
-    courses_xml = open('small_courses.xml').read()
+    courses_xml = open('fixtures/course.xml').read()
     courses = dtu_course_base.courses_from_xml(courses_xml, language)
     return courses[0]
 
