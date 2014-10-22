64d911130abc389a924446214d7a9453678a16c1
anderslime <aemilnielsen@gmail.com>
2014-10-22 18:07:56 +0200

    WIP: Logged in page

--- webapp.py
+++ webapp.py
@@ -1,4 +1,9 @@
-from flask import Flask, render_template
+import sys
+
+sys.path.append('cnapi')
+
+from flask import Flask, render_template, requ
+from cnapi import CampusNetApi
 
 
 app = Flask(__name__)
@@ -12,6 +17,19 @@ app.config.update(dict(
 @app.route('/')
 def login():
     return render_template('login.html')
+
+@app.route('/cv')
+def cv_page():
+    api = CampusNetApi(
+        'StudyPlanner3000',
+        '03157abb-4cb1-47c3-8b52-cdad0f82e78a',
+        student_id
+    )
+    api.authenticate('y72QxXj8')
+    if api.is_authenticated()
+        return render_template('cv.html', student_)
+    else:
+        return 'Wrong password', 403
 
 
 if __name__ == '__main__':
