6ee38bdbbe92e82217dc351e8ad72dbf1b743d64
anderslime <aemilnielsen@gmail.com>
2014-10-22 17:59:52 +0200

    Login page mockup

--- webapp.py
+++ webapp.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, render_template
 
 
 app = Flask(__name__)
@@ -10,8 +10,8 @@ app.config.update(dict(
 
 
 @app.route('/')
-def hello():
-    return 'Hello World!'
+def login():
+    return render_template('login.html')
 
 
 if __name__ == '__main__':
