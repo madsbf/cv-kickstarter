df4dacd1500a514e7241a875db1c3903d176d7c5
anderslime <aemilnielsen@gmail.com>
2014-10-22 16:47:56 +0200

    Test to see gs

--- webapp.py
+++ webapp.py
@@ -1,15 +1,6 @@
-import os
 from flask import Flask
 
-
 app = Flask(__name__)
-
-
-app.config.update(dict(
-    host='0.0.0.0',
-    debug=True,
-    port=int(os.environ.get('PORT', 5000))
-))
 
 
 @app.route('/')
