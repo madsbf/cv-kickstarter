6d518c135e92a4b4149066a37a20e65c1df8867c
anderslime <aemilnielsen@gmail.com>
2014-10-27 22:06:24 +0100

    Display grades in a simple list with ects points

--- cv_kickstarter/view_objects/user_cv.py
+++ cv_kickstarter/view_objects/user_cv.py
@@ -9,18 +9,24 @@ class UserCVBuilder(object):
     def build(self):
         return UserCV(
             self.user.first_name,
-            self.user.last_name
+            self.user.last_name,
+            self.grades
         )
 
     @cached_property
     def user(self):
         return self.campus_net_client.user()
 
+    @property
+    def grades(self):
+        return self.campus_net_client.grades()
+
 
 class UserCV(object):
-    def __init__(self, first_name, last_name):
+    def __init__(self, first_name, last_name, grades):
         self.first_name = first_name
         self.last_name = last_name
+        self.grades = grades
 
     @property
     def full_name(self):
