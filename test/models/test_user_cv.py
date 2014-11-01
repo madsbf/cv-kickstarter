be86cf550f1b226f554929fa0e839eb12f1f606f
anderslime <aemilnielsen@gmail.com>
2014-11-01 10:48:48 +0100

    Test UserCV class

---
+++ test/models/test_user_cv.py
@@ -0,0 +1,9 @@
+import sys
+sys.path.append('cv_kickstarter/models')
+
+from user_cv import UserCV
+
+
+def test_full_name_from_first_and_last_name():
+    user_cv = UserCV('Donald', 'Duck', [])
+    assert user_cv.full_name == 'Donald Duck'
