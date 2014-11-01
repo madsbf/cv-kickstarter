b25ca55ec5fbca8515b1a0f9762eeb19ed1909c9
anderslime <aemilnielsen@gmail.com>
2014-11-01 20:12:19 +0100

    Some more information for the setup

--- setup.py
+++ setup.py
@@ -7,6 +7,13 @@ from setuptools.command.test import test as TestCommand
 __name__ = 'CVKickstarter'
 __version__ = '0.1.0'
 __author__ = 'Mads Frandsen & Anders Nielsen'
+__author_email__ = 's082972@student.dtu.dk & aemilnielsen@gmail.com'
+__doc__ = """
+CVKickstarter is a web app that is able to analyse your DTU information
+and provide you with a prefilled CV with your education information, courses
+you've passed, the performance of your study, the skills you've gained and
+job suggestions based on your best skills.
+"""
 
 
 class ToxTestCommand(TestCommand):
@@ -24,7 +31,9 @@ setup(
     version=__version__,
     author=__author__,
     license='MIT',
+    description=__doc__,
     keywords='cv kickstarter dtu student',
+    long_description=open('README.rst').read(),
     install_requires=[
         'requests',
         'flask',
