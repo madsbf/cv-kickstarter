52effb3bf6300eae59dd06701ea0522830c4ecc0
anderslime <aemilnielsen@gmail.com>
2014-11-05 09:11:32 +0100

    Text chunkifier

---
+++ cv_kickstarter/models/text_chunkifier.py
@@ -0,0 +1,31 @@
+import nltk
+
+
+class TextChunkifier(object):
+    def text_chunks(self, text):
+        return self._extract_chunks(
+            self._grammar_parser().parse(
+                nltk.pos_tag(
+                    nltk.word_tokenize(text)
+                )
+            )
+        )
+
+    def _extract_chunks(self, tree, tree_filter='NP'):
+        chunks = []
+        if hasattr(tree, 'label'):
+            if tree.label() == tree_filter:
+                leafWords = map(self._extract_word_from_leaf, tree.leaves())
+                chunks = [" ".join(leafWords)]
+            else:
+                for child in tree:
+                    cs = self._extract_chunks(child, tree_filter=tree_filter)
+                    if cs != []:
+                        chunks.append(cs[0])
+        return chunks
+
+    def _extract_word_from_leaf(self, leaf):
+        return leaf[0].lower()
+
+    def _grammar_parser(self):
+        return nltk.RegexpParser("NP: { <JJ>*<NN.?>+ }")
