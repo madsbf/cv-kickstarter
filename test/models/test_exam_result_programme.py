5964429068bde237ee1d123e1d891ca9d75933f4
anderslime <aemilnielsen@gmail.com>
2014-11-14 23:41:10 +0100

    Move Ects average grade calculator into module

--- test/models/test_exam_result_programme.py
+++ test/models/test_exam_result_programme.py
@@ -1,7 +1,7 @@
 import sys
 sys.path.append('cv_kickstarter/models')
 
-from exam_result_programme import ExamResultProgramme, GradeAverageCalculator
+from exam_result_programme import ExamResultProgramme, ects_average_grade
 
 
 def test_is_done_when_passed_ects_is_equal_to_total_ects():
@@ -16,6 +16,6 @@ def test_is_not_done_when_passed_ects_is_equal_to_total_ects():
 
 def test_average_grade_calculated_with_calculator(monkeypatch):
     programme = ExamResultProgramme('ProgName', 70.0, 120.0, [])
-    monkeypatch.setattr(GradeAverageCalculator, 'average_grade',
+    monkeypatch.setattr(ects_average_grade, 'average_grade',
                         lambda x: 60.0)
     assert programme.average_grade == 60.0
