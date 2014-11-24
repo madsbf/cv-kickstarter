b56a8f7e13f63f32a21a9e6907f495778ab09f1a
anderslime <aemilnielsen@gmail.com>
2014-11-24 15:51:09 +0100

    Move course_keyword_tokenizer tests into same file

--- test/test_course_keywod_tokenizer.py
+++ test/test_course_keywod_tokenizer.py
@@ -1,7 +1,32 @@
-import sys
-sys.path.append('cv_kickstarter/models')
+from course_keyword_tokenizer import CourseSentenceExtractor, TextChunkifier
+from mock import MagicMock
 
-from course_keyword_tokenizer import TextChunkifier
+
+def test_extraction_of_sentences_from_course():
+    course = MagicMock(
+        title='02819 Data Mining using Python',
+        contents='The course starts with lectures that introduce the Python '
+                 + 'programming language. In the last part of the course '
+                 + 'the participants make a programming project.',
+        course_objectives_text='To give the participants knowledge about '
+                               + 'Python programming.',
+        course_objectives=[
+            "Design an application",
+            "Implement an application in Python",
+            "Implement code testing in Python for an application in Python",
+        ]
+    )
+    assert CourseSentenceExtractor(course).sentences() == [
+        '02819 Data Mining using Python',
+        'The course starts with lectures that introduce the Python ' +
+        'programming language.',
+        'In the last part of the course the participants make a '
+        + 'programming project.',
+        'To give the participants knowledge about Python programming.',
+        "Design an application",
+        "Implement an application in Python",
+        "Implement code testing in Python for an application in Python"
+    ]
 
 
 def assert_chunkification(text, chunks):
