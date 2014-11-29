9a2abd5828702efcf1228312933b86e060a96ea2
anderslime <aemilnielsen@gmail.com>
2014-11-29 18:29:12 +0100

    Support using MONGO_URL for importing courses

--- course_importer.py
+++ course_importer.py
@@ -2,11 +2,15 @@ from cv_kickstarter import dtu_course_base
 from cv_kickstarter import course_keyword_tokenizer
 from cv_kickstarter.course_repository import CourseRepository
 from cv_kickstarter.mongo_store import MongoStore
+from cv_kickstarter.cv_kickstarter_config import CvKickstarterConfig
 
 
 def import_courses():
     courses = dtu_course_base.courses_from_xml(open('courses.xml').read())
-    course_repo = CourseRepository(MongoStore('cv_kickstarter'))
+    config = CvKickstarterConfig()
+    course_repo = CourseRepository(
+        MongoStore('cv_kickstarter', config.mongo_url())
+    )
     index = 1
     course_size = len(courses)
     for course in courses:
