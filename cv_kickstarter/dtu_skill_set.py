9a836108a4e3848d47433627a5b67cd9a1bffe6d
anderslime <aemilnielsen@gmail.com>
2014-11-30 10:08:45 +0100

    Move CampusNetCourseBaseMerger into dtu_skill_set

--- cv_kickstarter/dtu_skill_set.py
+++ cv_kickstarter/dtu_skill_set.py
@@ -46,6 +46,66 @@ class DtuSkillSet(object):
             exam_result.course
         )
 
+
+class CampusNetCourseBaseMerger(object):
+
+    """Merges exam results with information about the course."""
+
+    def __init__(self, exam_result_programmes, course_base):
+        """Initialize with exam results and course base."""
+        self.exam_result_programmes = exam_result_programmes
+        self.course_base = course_base
+
+    def course_exam_results(self):
+        """Return course exam results.
+
+        A list of ExamResult, that contains exam result information and
+        more detailed information about the course given by the returned
+        course object from course base.
+        """
+        return list(map(
+            self._map_campus_net_exam_result,
+            self._campus_net_exam_results()
+        ))
+
+    def _map_campus_net_exam_result(self, exam_result):
+        return ExamResult(
+            exam_result.grade,
+            self.course_base.find_by_course_number(
+                exam_result.course_number
+            ),
+            exam_result.ects_points
+        )
+
+    def _campus_net_exam_results(self):
+        return reduce(
+            lambda x, y: x + y,
+            map(
+                lambda x: x.exam_results,
+                self.exam_result_programmes
+            ),
+            []
+        )
+
+
+class ExamResult(object):
+
+    """Exam result with grade, course and ects points."""
+
+    def __init__(self, grade, course, ects_points):
+        """Initialize with grade, course and ects_points."""
+        self.grade = grade
+        self.course = course
+        self.ects_points = ects_points
+
+    @property
+    def course_tokens(self):
+        """Return the tokens of the course."""
+        if self.course is None:
+            return []
+        return self.course.tokens
+
+
 TokenizedCourseExamResult = namedtuple(
     "TokenizedCourseExamResult",
     ['exam_result', 'tokens', 'course']
