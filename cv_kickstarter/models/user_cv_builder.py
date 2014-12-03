9df76fd1d593e1f8114cadd1347d6a52b5878486
anderslime <aemilnielsen@gmail.com>
2014-12-03 09:18:28 +0100

    Docstrings for UserCVBuilder

--- cv_kickstarter/models/user_cv_builder.py
+++ cv_kickstarter/models/user_cv_builder.py
@@ -1,3 +1,4 @@
+"""Builds a User CV to display on the frontend."""
 from werkzeug import cached_property
 import re
 from cv_kickstarter.models.user_cv import UserCV
@@ -8,11 +9,21 @@ from cv_kickstarter.models.exam_result_programme import ExamResultProgramme
 
 class UserCVBuilder(object):
 
+    """Builds a User CV to display on the frontend."""
+
     def __init__(self, campus_net_client, mongo_store):
+        """Initialize with campus_net_client and mongo_store.
+
+        The campus_net_client client need to be authenticated. It works out of
+        the box with the cnapi library.
+
+        The mongo_store is for storing the courses.
+        """
         self.campus_net_client = campus_net_client
         self.mongo_store = mongo_store
 
     def build(self):
+        """Return a UserCV."""
         return UserCV(
             self.user.first_name,
             self.user.last_name,
@@ -22,10 +33,12 @@ class UserCVBuilder(object):
 
     @cached_property
     def user(self):
+        """Return a user with information from the campus_net_client."""
         return self.campus_net_client.user()
 
     @cached_property
     def grades(self):
+        """Return grades from campus_net_client."""
         exam_result_programmes = map(
             self._map_exam_result_programme,
             self.campus_net_client.grades()
@@ -50,10 +63,15 @@ class UserCVBuilder(object):
 
 
 class CampusNetExamResultMapper(object):
+
+    """Maps an exam result programme to an object ready for frontend."""
+
     def __init__(self, cn_exam_result_programme):
+        """Initialize with cn_exam_result_programme."""
         self.cn_exam_result_programme = cn_exam_result_programme
 
     def mapped_exam_result(self):
+        """Return mapped exam result."""
         if self._is_without_education_programme():
             return None
         return ExamResultProgramme(
