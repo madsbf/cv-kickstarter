cf9b406888e7da85968a097d926d184895dbec34
anderslime <aemilnielsen@gmail.com>
2014-12-03 09:28:27 +0100

    Docstrings for exam result programme

--- cv_kickstarter/models/exam_result_programme.py
+++ cv_kickstarter/models/exam_result_programme.py
@@ -1,8 +1,13 @@
+"""The exam result programme with exam results for a given programme."""
 from cv_kickstarter import ects_grade_calculator
 
 
 class ExamResultProgramme(object):
+
+    """Representing exam results for a given programme (bachelor, master..)."""
+
     def __init__(self, name, passed_ects, total_ects, exam_results):
+        """Initialize with name, passed_ects, total_ects and exam_results."""
         self.name = name
         self.passed_ects = passed_ects
         self.total_ects = total_ects
@@ -10,10 +15,12 @@ class ExamResultProgramme(object):
 
     @property
     def is_done(self):
+        """Return true if all the courses are passed for the programme."""
         return self._has_passed(self.total_ects)
 
     @property
     def average_grade(self):
+        """Return the average grade of the student."""
         return ects_grade_calculator.average_grade(self.exam_results)
 
     def _has_passed(self, ects_points):
